4e4e4d7afbddec1fc4ac0b85602482a4748f1059
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-06 09:39:33 +0200

    adds visual

--- src/executor.rs
+++ src/executor.rs
@@ -1,6 +1,9 @@
+use crate::key::Key;
 use once_cell::sync::Lazy;
 use std::{
+  collections::{BTreeMap, BTreeSet},
   future::Future,
+  ops::DerefMut,
   pin::Pin,
   sync::{Arc, Mutex},
   task::{Context, Wake, Waker},
@@ -9,12 +12,19 @@ use std::{
 pub static MANUAL_EXECUTOR: Lazy<Arc<ManualExecutor>> = Lazy::new(ManualExecutor::new);
 
 #[no_mangle]
-extern "C" fn wake() {
-  MANUAL_EXECUTOR.wake()
+extern "C" fn wake_all() {
+  MANUAL_EXECUTOR.wake_all();
 }
 
+#[no_mangle]
+extern "C" fn wake(key: Key) {
+  MANUAL_EXECUTOR.wake(key);
+}
+
+type Task = Pin<Box<dyn Future<Output = ()> + Send>>;
+
 pub struct ManualExecutor {
-  tasks: Mutex<Vec<Pin<Box<dyn Future<Output = ()> + Send>>>>,
+  tasks: Mutex<BTreeMap<Key, Task>>,
 }
 
 impl ManualExecutor {
@@ -24,45 +34,73 @@ impl ManualExecutor {
     })
   }
 
-  pub fn spawn(&self, task: impl Future<Output = ()> + Send + 'static) {
-    let mut tasks = self.tasks.lock();
-    let tasks = tasks.as_mut().unwrap();
-    tasks.push(Box::pin(task));
+  pub fn spawn_wake(self: &Arc<Self>, task: impl Future<Output = ()> + Send + 'static) -> Key {
+    let key = self.spawn(task);
+    self.wake(key);
+    key
   }
 
-  pub fn wake(self: &Arc<Self>) {
-    let wake = ManualWake::new(self.clone());
-    let waker = Waker::from(wake.clone());
-    let mut cx = Context::from_waker(&waker);
-
+  pub fn spawn(&self, task: impl Future<Output = ()> + Send + 'static) -> Key {
+    let key = Key::new();
     let mut tasks = self.tasks.lock();
-    let tasks = tasks.as_deref_mut().unwrap();
+    let tasks = tasks.as_mut().unwrap();
+    assert!(tasks.insert(key, Box::pin(task)).is_none());
+    key
+  }
 
-    let mut tasks_new = Vec::new();
-    while let Some(mut task) = tasks.pop() {
-      match task.as_mut().poll(&mut cx) {
-        std::task::Poll::Ready(_) => {}
-        std::task::Poll::Pending => tasks_new.push(task),
-      };
+  pub fn wake(self: &Arc<Self>, key: Key) {
+    let mut tasks = self.tasks.lock().unwrap();
+    let tasks = tasks.deref_mut();
+
+    let Some(mut task) = tasks.remove(&key) else {
+      return;
+    };
+
+    let pending = self.poll_task(key, &mut task);
+    if pending {
+      assert!(tasks.insert(key, task).is_none());
+    }
+  }
+
+  pub fn wake_all(self: &Arc<Self>) {
+    let mut tasks = self.tasks.lock().unwrap();
+    let tasks = tasks.deref_mut();
+    let mut tasks_new = BTreeMap::new();
+
+    while let Some((key, mut task)) = tasks.pop_first() {
+      let pending = self.poll_task(key, &mut task);
+      if pending {
+        assert!(tasks_new.insert(key, task).is_none());
+      }
     }
 
     *tasks = tasks_new;
+  }
+
+  fn poll_task(self: &Arc<Self>, key: Key, task: &mut Task) -> bool {
+    let wake = ManualWake::new(self.clone(), key);
+    let waker = Waker::from(wake.clone());
+    let mut context = Context::from_waker(&waker);
+
+    let poll_result = task.as_mut().poll(&mut context);
+    poll_result.is_pending()
   }
 }
 
 struct ManualWake {
   executor: Arc<ManualExecutor>,
+  key: Key,
 }
 
 impl ManualWake {
-  fn new(executor: Arc<ManualExecutor>) -> Arc<Self> {
-    Arc::new(Self { executor })
+  fn new(executor: Arc<ManualExecutor>, key: Key) -> Arc<Self> {
+    Arc::new(Self { executor, key })
   }
 }
 
 impl Wake for ManualWake {
   fn wake(self: Arc<Self>) {
-    self.executor.clone().wake()
+    self.executor.clone().wake(self.key)
   }
 }
 
@@ -90,7 +128,7 @@ mod tests {
       });
     }
 
-    MANUAL_EXECUTOR.wake();
+    MANUAL_EXECUTOR.wake_all();
 
     let actual = set.lock().unwrap();
     let expected: HashSet<_> = ["a", "b"].into();
